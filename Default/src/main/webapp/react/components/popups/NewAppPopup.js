5995c25afc0e5be7e36a3554eef904ef1c083837
Amandeep <amandeep.santokh@full.co>
2019-06-21 21:15:25 +0530

    added close button for new app popup

--- Default/src/main/webapp/react/components/popups/NewAppPopup.js
+++ Default/src/main/webapp/react/components/popups/NewAppPopup.js
@@ -2,12 +2,21 @@ import React from 'react';
 
 export default class NewAppPopup extends React.Component {
 
+    constructor(props){
+        super(props);
+        this.handlePopupClose = this.handlePopupClose.bind(this);
+    }
     render() {
         return (
 
          <div className="new-app-popup">
-          <div className="close">x</div>  
+          <div className="close" onClick={this.handlePopupClose}>x</div>  
          </div> 
         )
+      }
+
+      handlePopupClose(){
+        document.getElementsByClassName("overlay")[0].style.display = "none";
+        document.getElementsByClassName("new-app-popup")[0].style.display = "none" 
       }
     }
