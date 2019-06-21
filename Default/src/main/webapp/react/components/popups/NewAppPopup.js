2496ebbfea585e3b7a481a23d605728b13a1e7c9
Amandeep <amandeep.santokh@full.co>
2019-06-22 00:39:24 +0530

    add app popup is done

--- Default/src/main/webapp/react/components/popups/NewAppPopup.js
+++ Default/src/main/webapp/react/components/popups/NewAppPopup.js
@@ -10,7 +10,14 @@ export default class NewAppPopup extends React.Component {
         return (
 
          <div className="new-app-popup">
-          <div className="close" onClick={this.handlePopupClose}>x</div>  
+          <div className="close" onClick={this.handlePopupClose}>x</div>
+        <h3 className="app-text">New App</h3>
+            <p className="app-text">Projects are a way to specify custom branding related information and also to group certain functionalities for individual product within a company</p>
+           <hr></hr>
+           <span className="app-text" >Display Name </span><input class  type="text"/><br/>
+           <span className="app-text" >Unique Name </span><input type = "text"/><br/>
+           <hr></hr>
+           <button className="create-app-btn">Create App</button> 
          </div> 
         )
       }
